9b63b40aea34238b46e4fae20205b7419a878b11
Heng-Jui Chang <b06901020@ntu.edu.tw>
2022-07-19 16:29:15 +0800

    fix setup error

--- egs/librispeech/test.sh
+++ egs/librispeech/test.sh
@@ -7,7 +7,7 @@ run_asr.py \
     --config config/ctc_test_960.yaml \
     --test \
     --override " \
-            args.data.dev_paths=['/work/harry87122/dataset/miniasr_data/test-other/data_list_sorted.json'],, \
-            args.decode.type='beam' \
+        args.data.dev_paths=['/work/harry87122/dataset/miniasr_data/test-other/data_list_sorted.json'],, \
+        args.decode.type='beam' \
         " \
     --ckpt model/ctc_hubert_base_LS960_char/epoch=15-step=140623.ckpt
